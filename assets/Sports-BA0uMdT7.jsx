b11c52d11162612f580489394fd49096689ad224
aqyang28 <aqyang@unc.edu>
2026-01-21 16:26:24 -0500

    Updates

---
+++ assets/Sports-BA0uMdT7.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import FadeInImage from '../components/FadeInImage';
+import './styles.css';
+
+export default function Sports() {
+  return (
+    <div className="page-container">
+      <section className="page-text-section">
+        <h1>Sports</h1>
+        <p>
+          I love the action and emotion that comes with photographing sports as well as the creative liberty you have in editing to elevate your work—it's definitely the medium I want to pursue most heavily in the coming years. Below is a collection of photos I have taken at sports events!
+        </p>
+      </section>
+
+      <div className="page-divider" />
+      
+      <section className="page-images-section">
+        <div className="page-grid page-grid-16-rows">
+          <FadeInImage src="src/images/sports/sp1.jpg" alt="UNC Football quarterback Gio Lopez (7) raising his arms in celebration after his teammate scores a touchdown." />
+          <FadeInImage src="src/images/sports/sp2.jpg" alt="UNC Football linebacker Khmori House (7) lines up before the snap." />
+          <FadeInImage src="src/images/sports/sp3.jpg" alt="UNC Football sophomore defensive back Jaiden Patterson (18) celebrates with his teammates after intercepting a pass" />
+          <FadeInImage src="src/images/sports/sp4.jpg" alt="UNC Football linebacker makes a tackle on a Richmond Football player." />
+          <FadeInImage src="src/images/sports/sp5.jpg" alt="Two UNC Football players rally for a tackle on a Richmond Football player." />
+          <FadeInImage src="src/images/sports/sp6.jpg" alt="UNC Football running back De'mon June (35) runs with the ball into the sunlight." />
+          <FadeInImage src="src/images/sports/sp7.jpg" alt="UNC Football head coach Bill Belichick gestures on the sideline during a game." />
+          <FadeInImage src="src/images/sports/sp8.jpg" alt="UNC Football quarterback Gio Lopez (7) celebrates with his teammates in the end zoneafter scoring a touchdown." />
+          <FadeInImage src="src/images/sports/sp9.jpg" alt="UNC Football receiver Jordan Shipp(1) celebrates after making a first down catch." />
+          <FadeInImage src="src/images/sports/sp10.jpg" alt="UNC Football quarterback Gio Lopez (7) scrambles with the ball while an opposing Duke Football player dives to tackle him." />
+          <FadeInImage src="src/images/sports/sp11.jpg" alt="UNC Football receiver Kobe Paysour (8) runs after catching the ball while an opposing Duke Football player dives to tackle him." />
+          <FadeInImage src="src/images/sports/sp12.jpg" alt="UNC sophomore running back Davion Gause (37) runs the ball during the football game against Duke at Kenan Stadium." />
+          <FadeInImage src="src/images/sports/sp13.jpg" alt="UNC redshirt first-year defensive lineman D'Antre Robinson (6) celebrates after making a tackle during the football game against Duke at Kenan Stadium on Saturday, Nov. 22, 2025. UNC lost 32-25." />
+          <FadeInImage src="src/images/sports/sp14.jpg" alt="UNC sophomore wide receiver Jordan Shipp (1) celebrates after scoring a touchdown during the football game against Duke at Kenan Stadium on Saturday, Nov. 22, 2025. UNC lost 32-25." />
+          <FadeInImage src="src/images/sports/sp15.jpg" alt="UNC redshirt senior cornerback Thaddeus Dixon (1) celebrates after making a tackle during the football game against Duke at Kenan Stadium on Saturday, Nov. 22, 2025. UNC lost 32-25." />
+          <FadeInImage src="src/images/sports/sp18.jpg" alt="A black & white photograph of UNC Basketball forward Caleb Wilson (8) looking upwards towards the ball after a teammate shoots a free throw." />
+          <FadeInImage src="src/images/sports/sp16.jpg" alt="A black & white photograph of UNC Basketball forward Caleb Wilson (8) at the free throw line." />
+          <FadeInImage src="src/images/sports/sp17.jpg" alt="A black & white photograph of UNC Basketball forward Caleb Wilson (8) at the free throw line." />
+          <FadeInImage src="src/images/sports/sp19.jpg" alt="UNC first-year forward Caleb Wilson (8) and UNC junior guard Kyan Evans (0) leap for a block during a basketball game against Radford at the " />
+          <FadeInImage src="src/images/sports/sp20.jpg" alt="UNC junior guard Jaydon Young (4) shoots the ball during the game against Radford at the Dean E. Smith Center on Tuesday, Nov. 11, 2025. UNC won 89-74." />
+          <FadeInImage src="src/images/sports/sp34.jpg" alt="UNC junior center Henri Veesaar (13) fights for a rebound during a basketball game against Radford at the Dean E. Smith Center on Tuesday, Nov. 11, 2025." />
+          <FadeInImage src="src/images/sports/sp21.jpg" alt="UNC junior guard Luka Bogavac (44) dribbles the ball during the game against Radford at the Dean E. Smith Center on Tuesday, Nov. 11, 2025. UNC won 89-74." />
+          <FadeInImage src="src/images/sports/sp35.jpg" alt="UNC sophomore guard/forward Jonathan Powell (11) defends the ball during a basketball game against Radford at the Dean E. Smith Center on Tuesday, Nov. 11, 2025." />
+          <FadeInImage src="src/images/sports/sp36.jpg" alt="The UNC men's basketball team watches a free throw from a flagrant foul during a basketball game against Radford at the Dean E. Smith Center on Tuesday, Nov. 11, 2025. Pictured from left to right are:UNC sophomore guard/forward Jonathan Powell (11), UNC junior guard Luka Bogavac (44), UNC first-year forward Caleb Wilson (8), UNC junior guard Kyan Evans (0), and UNC junior center Henri Veesaar (13)" />
+          <FadeInImage src="src/images/sports/sp22.jpg" alt="UNC senior guard Seth Trimble (7) smiles with UNC senior guard Elijah Davis (4) on the bench during basketball game against Radford at the Dean E. Smith Center on Tuesday, Nov. 11, 2025. Trimble's left arm is in a cast after he broke a bone in his left forearm on Sunday afternoon." />
+          <FadeInImage src="src/images/sports/sp23.jpeg" alt="Three UNC women's basketball players contest a shot on defense against an NC State player wearing number 35 during an ACC Women’s Basketball Tournament semifinal game against NC State on March 8, 2025, at First Horizon Coliseum." />
+          <FadeInImage src="src/images/sports/sp24.jpeg" alt="UNC women's basketball head coach Courtney Banghart claps at her players on the court from the sideline, encouraging them against NC State on March 8, 2025, at First Horizon Coliseum." />
+          <FadeInImage src="src/images/sports/sp25.jpeg" alt="UNC sophomore guard Reniya Kelly (10) dribbles the ball during the game against NC State on March 8, 2025, at First Horizon Coliseum." />
+          <FadeInImage src="src/images/sports/sp26.jpeg" alt="The UNC women's basketball team huddles after a foul call during an ACC Women’s Basketball Tournament semifinal game against NC State on March 8, 2025, at First Horizon Coliseum. Head coach Courtney Banghart is seen on the sideline." />
+          <FadeInImage src="src/images/sports/sp27.jpeg" alt="A fan wearing a Duke t-shirt and a Duke-blue wig waves a small Duke flag from the stands of First Horizon Coliseum in Greensboro, North Carolina. Duke played Notre Dame in the ACC Women's Basketball Tournament semifinals on March 8, 2025." />
+          <FadeInImage src="src/images/sports/sp28.jpg" alt="UNC redshirt junior opposite hitter Chelsea Thorpe (77) celebrates a block kill during the game against Bucknell in Carmichael Arena on Friday, Sept. 5, 2025. UNC won 3-1." />
+          <FadeInImage src="src/images/sports/sp29.jpg" alt="UNC first-year setter Sienna Cozza (8) sets the ball during the game against Bucknell in Carmichael Arena on Friday, Sept. 5, 2025. UNC sophomore middle blocker Jackie Taylor (21) prepares her run-up to meet the set. UNC won 3-1." />
+          <FadeInImage src="src/images/sports/sp30.jpg" alt="UNC redshirt junior middle blocker Lauren Schutter (3) celebrates a block kill in the team huddle during the game against Bucknell in Carmichael Arena on Friday, Sept. 5, 2025. UNC won 3-1." />
+          <FadeInImage src="src/images/sports/sp31.jpg" alt="UNC junior Kaya Forbes celebrates with her teammates after performing on vault during the gymnastics meet against Western Michigan at Carmichael Arena on Friday, March 15, 2025. UNC won 196.750-195.275." />
+          <FadeInImage src="src/images/sports/sp32.jpg" alt="UNC sophomore Jessica Naranjo celebrates with her teammates after performing on balance beam during the gymnastics meet against Western Michigan at Carmichael Arena on Friday, March 15, 2025. UNC won 196.750-195.275." />
+          <FadeInImage src="src/images/sports/sp33.jpg" alt="UNC sophomore Neve King performs a routine on balance beam during the gymnastics meet against Western Michigan at Carmichael Arena on Friday, March 15, 2025. UNC won 196.750-195.275." />
+
+          <FadeInImage
+            src="src/images/sports/sp_v1.jpg"
+            alt="Notre Dame sophomore guard Hannah Hidalgo (3) flexes in celebration after one of her teammates scores during a basketball game against Duke on March 8, 2025, at First Horizon Coliseum in Greensboro, North Carolina."
+            style={{ gridColumn: '1 / 2', gridRow: '13 / 15' }}
+          />
+          <FadeInImage
+            src="src/images/sports/sp_v2.jpeg"
+            alt="NC State senior guard Aziaha James (10) prepares for a free throw during an ACC Women’s Basketball Tournament semifinal game against UNC on March 8, 2025, at First Horizon Coliseum."
+            style={{ gridColumn: '2 / 3', gridRow: '13 / 15' }}
+          />
+          <FadeInImage
+            src="src/images/sports/sp_v0.jpg"
+            alt="UNC first-year forward Caleb Wilson (8) looks up at the rim in preparation to shoot a free throw during a basketball game against Radford at the Dean E. Smith Center on Tuesday, Nov. 11, 2025. UNC won 89-74."
+            style={{ gridColumn: '3 / 4', gridRow: '13 / 15' }}
+          />
+          <FadeInImage
+            src="src/images/sports/sp_v3.jpg"
+            alt="UNC sophomore Neve King sticks her landing on balance beam during the gymnastics meet against Western Michigan at Carmichael Arena on Friday, March 15, 2025. UNC won 196.750-195.275."
+            style={{ gridColumn: '1 / 2', gridRow: '15 / 17' }}
+          />
+          <FadeInImage
+            src="src/images/sports/sp_v4.jpg"
+            alt="UNC sophomore Neve King sticks her landing and smiles with her arms raised on balance beam during the gymnastics meet against Western Michigan at Carmichael Arena on Friday, March 15, 2025. UNC won 196.750-195.275."
+            style={{ gridColumn: '2 / 3', gridRow: '15 / 17' }}
+          />
+          <FadeInImage
+            src="src/images/sports/sp_v5.jpg"
+            alt="UNC sophomore Neve King sticks her landing, smiles with her arms raised, and looks over to her coach on balance beam during the gymnastics meet against Western Michigan at Carmichael Arena on Friday, March 15, 2025. UNC won 196.750-195.275."
+            style={{ gridColumn: '3 / 4', gridRow: '15 / 17' }}
+          />
+        </div>
+      </section>
+    </div>
+  );
+} 
